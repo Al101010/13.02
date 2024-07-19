90f3e842b51c70f3a5a408ab98cf9156133aab63
alexgithub <alexgithub@yandex.ru>
2024-07-19 10:16:34 +1000

    commit-8 вернул страницу-html и файлы-js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
-console.log('Привет!');
+// TODO: write code here
+
+// comment this to pass build
+const unusedVariable = "variable";
+
+// for demonstration purpose only
+export default function demo(value) {
+  return `Demo: ${value}`;
+}
+
+console.log("app.js included");
